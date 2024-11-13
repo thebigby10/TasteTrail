3fa4391a028543e3abbf66a6cc6eeb4c9d39e882
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-11-14 01:28:22 +0600

    /user/register api created

--- backend/TasteTrail_backend/user/models.py
+++ backend/TasteTrail_backend/user/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class user(models.Model):
-    full_name = models.CharField(max_length=100, null = False)
-    email = models.CharField(max_length=100, unique = True)
-    profile_pic_url = models.URLField(max_length=1000)
-
-    def __str__(self):
-        return self.full_name
+class User(models.Model):
+    fullName = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    imageUrl = models.URLField(max_length=1000)
