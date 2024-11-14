26f16b27b4f6d4b1d26821c0d98afccb586eae0f
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-11-14 16:49:39 +0600

    added recipe model from ahir branch

--- backend/TasteTrail_backend/recipe/models.py
+++ backend/TasteTrail_backend/recipe/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    imgUrl = models.URLField(max_length=1000)
+    tags = models.TextField()
+    location = models.TextField()
+    ingredients = models.TextField()
+    description = models.TextField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    comments = models.JSONField(default=list)
