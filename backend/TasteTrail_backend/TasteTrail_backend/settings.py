fdd285ba1c40ba7da7104d1c566a88ab603098cd
iamcharlie17 <riyadhosen40@gmail.com>
2024-11-14 15:15:55 +0600

    fefj

--- backend/TasteTrail_backend/TasteTrail_backend/settings.py
+++ backend/TasteTrail_backend/TasteTrail_backend/settings.py
@@ -78,9 +78,14 @@ WSGI_APPLICATION = 'TasteTrail_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tastetrail_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST':'localhost', #needs update
+        'PORT':'3306'
     }
+    #create a mysql db with tastetrail_db name that have access for admin:admin
 }
 
 
