02f23173892a57d5e1107654d2f069a951b9f287
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-11-15 22:59:28 +0600

    added post creation time in models.py

--- backend/TasteTrail_backend/recipe/models.py
+++ backend/TasteTrail_backend/recipe/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from user.models import User
 import uuid
 
+import datetime
+
 # Create your models here.
 class Recipe(models.Model):
     postID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
@@ -15,6 +17,8 @@ class Recipe(models.Model):
     likes = models.JSONField(default=list)
     dislikes = models.JSONField(default=list)
     comments = models.JSONField(default=list)
+    creation_time = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.title
