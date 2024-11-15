677a425b87f939753d72bf8ab4ee384a111a53f7
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-11-15 11:35:22 +0600

    implemented delete functionality

--- backend/TasteTrail_backend/recipe/urls.py
+++ backend/TasteTrail_backend/recipe/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('add/', views.add, name = 'add'),
     path('', views.all_post, name = 'all_post'),
-    path('<uuid:post_id>', views.get_post, name = 'get_post' )
+    path('<uuid:post_id>', views.get_post, name = 'get_post' ),
+    path('delete/<uuid:post_id>', views.delete, name = 'delete'),
 ]
--- backend/TasteTrail_backend/recipe/views.py
+++ backend/TasteTrail_backend/recipe/views.py
@@ -47,3 +47,10 @@ def get_post(request, post_id):
         recipe = Recipe.objects.get(postID = post_id)
         recipe_dict = model_to_dict(recipe)
         return JsonResponse(recipe_dict, status=200, safe=False)
+
+@csrf_exempt
+def delete(request, post_id):
+    if request.method == 'DELETE':
+        recipe = Recipe.objects.get(postID = post_id)
+        recipe.delete()
+        return HttpResponse(status=200)
