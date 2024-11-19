3e7f8941998230ac5a9535e0286272a5b2f1848d
iamcharlie17 <riyadhosen40@gmail.com>
2024-11-19 12:50:03 +0600

    add: loading comp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,27 @@ import { useEffect, useState } from "react";
 import { fetchRecipes } from "./utils/api";
 import Recipes from "./pages/Recipe/Recipes";
 import useRefetch from "./hooks/useRefetch";
+import Loading from "./components/Loading";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
   const [control, setControl] = useState(true);
-
+  const [loading, setLoading] = useState(false);
   const {refetch} = useRefetch();
 
 
   const fetch = async () => {
+    setLoading(true)
     const data = await fetchRecipes();
     setRecipes(data.data);
+    setLoading(false)
   };
 
   useEffect(() => {
     fetch();
   }, [control, refetch]);
+
+  if(loading) return <Loading/>
 
   return (
     <>
--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import Loading from "../components/Loading";
 
 const PrivateRoute = ({ children }) => {
   const location = useLocation();
   const { user, loading } = useAuth();
   
-  if (loading) return <div>Loading...</div>;
+  if (loading) return <div><Loading/></div>;
   if (!user) return <Navigate to={"/login"} state={location.pathname} />;
   return children;
 };
